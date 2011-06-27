93e9f84484598a4f7d16e9fd700b40dfecd6ce2b
Minus <minustehbare@gmail.com>
2011-06-27 14:59:51 -0400

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StatusDashboardPublic::Application.config.session_store :cookie_store, :key => '_status_dashboard_public_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StatusDashboardPublic::Application.config.session_store :active_record_store
